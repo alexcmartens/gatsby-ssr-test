908a7e88683dbaeacf1d3e2335bf820563315517
Alex Martens <alex_martens@trimble.com>
2023-05-26 15:13:49 -0500

    Environment vars

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,26 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+require("dotenv").config({
+  path: `.env`,
+})
+
+console.log('CONTENTFUL_ACCESS_TOKEN', process.env.CONTENTFUL_ACCESS_TOKEN)
+console.log('CONTENTFUL_SPACE_ID', process.env.CONTENTFUL_SPACE_ID)
+console.log('CONTENTFUL_BRANCH', process.env.CONTENTFUL_BRANCH)
+
 module.exports = {
   siteMetadata: {
-    title: `ssr-test`,
+    title: `gatsby-ssr-test`,
     siteUrl: `https://www.yourdomain.tld`
   },
   plugins: [{
     resolve: 'gatsby-source-contentful',
     options: {
-      "accessToken": "UI_9usOR-U7-g4Z42ivbieDpvjGMVoPVBogZ0CEMK3A",
-      "spaceId": ""
+      "accessToken": process.env.CONTENTFUL_ACCESS_TOKEN,
+      "spaceId": process.env.CONTENTFUL_SPACE_ID,
+      "environment": process.env.CONTENTFUL_BRANCH
     }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-postcss"]
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-postcss", "gatsby-plugin-netlify"]
 };
