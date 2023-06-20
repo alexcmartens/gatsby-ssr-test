0a0234befd76b6ff9be4bf9f78e903950bc8c2e7
Alex Martens <alex_martens@trimble.com>
2023-06-20 14:54:00 -0500

    Updates to the block list items

--- src/compositions/connectors/BlockListItemsConnector.js
+++ src/compositions/connectors/BlockListItemsConnector.js
@@ -1,44 +1,70 @@
 import React from "react"
 import { Link } from "gatsby"
 import { marked } from "marked"
-import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+import { documentToHtmlString } from "@contentful/rich-text-html-renderer"
 import { BlockListItems } from "terra-one"
 import renderPaddingOrFullBleed from "../../utils/organism-formatting/renderPaddingOrFullBleed"
+import formatLottieAnimation from "../../utils/organism-formatting/formatLottieAnimation"
 import formatFocusedImage from "./utils/formatFocusedImage"
 import formatUrl from "./utils/formatUrl"
 import formatIcons from "../../utils/organism-formatting/formatIcons"
+import formatTheme from "../../utils/organism-formatting/formatTheme"
+import ColorSchemeConnector from "../../components/ColorSchemeConnector/ColorSchemeConnector"
 
 const isExternal = data => data?.sys?.contentType?.sys?.id === "externalLink"
 
 const cardTypes = {
-  'Unbounded Cards': 'unbounded_cards',
-  'Bounded Cards': 'bounded_cards',
-  'Icon Mini Blocks': 'icon_mini_blocks'
+  "Unbounded Cards": "unbounded_cards",
+  "Bounded Cards": "bounded_cards",
+  "Icon Mini Blocks": "icon_mini_blocks",
 }
 
-const formatCards = (data) => {
+const formatCards = data => {
+  const theme = ColorSchemeConnector(data?.fields?.colorScheme)
+  const formattedTheme = formatTheme({ ...data?.fields, ...theme })
+
   return {
+    centered: data?.fields?.blockAlignment === "Centered",
     cardAlignment: data?.fields?.cardAlignment,
     displayButton: data?.fields?.linkType,
     buttonSecondary: false,
+    theme: formattedTheme,
     cards: data?.fields?.listBlocks.map(d => {
+      const image =
+        d?.fields?.cardImage?.sys?.contentType?.sys?.id === "lottieAnimation"
+          ? formatLottieAnimation(
+              d?.fields?.cardImage?.fields?.srcLink ||
+                d?.fields?.cardImage?.fields?.json ||
+                null,
+              { height: "100%", display: "block" },
+              null
+            )
+          : formatFocusedImage(d?.fields?.cardImage, d?.fields?.header)
       return {
-        image: formatFocusedImage(d?.fields?.cardImage, d?.fields?.header),
+        image: image,
         eyebrow: d?.fields?.eyebrow,
         header: d?.fields?.header,
-        text: d?.fields?.textContent?.fields?.content ? documentToHtmlString(d?.fields?.textContent?.fields?.content) : d?.fields?.textContent?.fields?.blob ?  marked.parse(d?.fields?.textContent?.fields?.blob) : '',
-        cardLink: {
-          url: formatUrl(d?.fields?.cardLink?.fields?.url),
-          text: d?.fields?.cardLink?.fields?.text,
-          external: isExternal(d?.fields?.cardLink?.fields?.url),
-        },
+        text: d?.fields?.textContent?.fields?.content
+          ? documentToHtmlString(d?.fields?.textContent?.fields?.content)
+          : d?.fields?.textContent?.fields?.blob
+          ? marked.parse(d?.fields?.textContent?.fields?.blob)
+          : "",
+        cardLink: d?.fields?.cardLink
+          ? {
+              url: formatUrl(d?.fields?.cardLink?.fields?.url),
+              text:
+                d?.fields?.cardLink?.fields?.text ||
+                d?.fields?.cardLink?.fields?.url?.fields?.text,
+              external: isExternal(d?.fields?.cardLink?.fields?.url),
+            }
+          : null,
         modalType: "Image",
       }
-    })
+    }),
   }
 }
 
-const formatIconMiniBlocks = (data) => {
+const formatIconMiniBlocks = data => {
   return {
     alignment: data?.fields?.blockAlignment === "Centered" ? "center" : "left",
     largeBlock: data?.fields?.blockSize === "Large",
@@ -51,15 +77,15 @@ const formatIconMiniBlocks = (data) => {
         Link: null,
       }
     }),
-    Link: Link
+    Link: Link,
   }
 }
 
 function BlockListItemsConnector(props) {
   const data = props?.contentfulEntry ? props?.contentfulEntry : props?.data
 
-  let content;
-  let componentMargin = true;
+  let content
+  let componentMargin = true
 
   switch (data?.fields?.variant) {
     case "Icon Mini Blocks":
@@ -69,20 +95,20 @@ function BlockListItemsConnector(props) {
     default:
       content = formatCards(data)
       break
-  } 
+  }
 
   return (
     <>
       {renderPaddingOrFullBleed(
         <BlockListItems
-          type={ cardTypes[data?.fields?.variant] }
-          content={ content }
+          type={cardTypes[data?.fields?.variant]}
+          content={content}
         />,
         true,
         true,
         true,
         false,
-        componentMargin        
+        componentMargin
       )}
     </>
   )
